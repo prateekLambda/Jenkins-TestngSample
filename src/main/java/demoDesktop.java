9abd17240d2903d318fcc46d8e36cad71fec7c26
prateekLambda <prateeks@lambdatest.com>
2021-09-15 00:59:30 +0530

    Print Browser env

--- src/main/java/demoDesktop.java
+++ src/main/java/demoDesktop.java
@@ -28,6 +28,7 @@ public class demoDesktop {
         //  Collection c= new ArrayList(System.getenv("LT_BROWSERS"));
 
         String browsers = System.getenv("LT_BROWSERS");
+        System.out.println("Browsers+++++++++++++++++++++" + browsers);
 
       /*  for (int i = 0; i < browsers.length; i++) {//length is the property of the array
             System.out.println(browsers[i]);
